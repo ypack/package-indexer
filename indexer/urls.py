4d9ea2e9456ab4b1f5e31104f37b788ba93e5ef7
Javi <nhitbh@gmail.com>
2018-07-11 18:30:31 +0200

    Change packages base path

--- indexer/urls.py
+++ indexer/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('packages/', include('packages.urls'))
+    path('', include('packages.urls'))
 ]
